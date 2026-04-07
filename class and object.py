1f389ee8d987d4ef703447808e8474a6de055100
Naveenkiran@123 <mettulanaveenkiran@gmail.com>
2026-04-07 15:57:41 +0530

    Add files via upload

---
+++ class and object.py
@@ -0,0 +1,9 @@
+class dog:
+    attr1="puppy"
+    attr2="snoopy"
+    def fun(self):
+        print("this is",self.attr1)
+        print("this is",self.attr2)
+tomy=dog()
+print(tomy.attr1)
+tomy.fun()
